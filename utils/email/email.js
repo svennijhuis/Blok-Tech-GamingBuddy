a4f5a7b1a93eb9589c78ba0bc19ddc1fc23538ec
svennijhuis <sanijhuis@live.nl>
2022-03-31 15:44:00 +0200

    [added] voeg brach toe

--- utils/email/email.js
+++ utils/email/email.js
@@ -4,8 +4,8 @@ sgMail.setApiKey(SENDGRID_API_KEY)
  
   const sendWelcomeEmail = (name, email) => {
     sgMail.send({
-        to: email, // Change to your recipient
-        from: 'sven.nijhuis@hva.nl', // Change to your verified sender
+        to: email,
+        from: 'sven.nijhuis@hva.nl', 
         subject: 'Welkom bij de Gmaebuddy',
         text: 'and easy to do anywhere, even with Node.js',
         html: `hallo, ${name}`,
