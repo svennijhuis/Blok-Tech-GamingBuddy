2b59523cad025d25ee391ddcc1f70e1f77c590c3
Nina Vens <nina.vens@hva.nl>
2022-04-01 12:35:11 +0200

    [added] authentication

--- routes/roomSelect.js
+++ routes/roomSelect.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
+
+const session = require('express-session');
+const passport = require('passport');
+router.use(session({ 
+  secret: 'process.env.SESSION_SECRET', 
+  saveUninitialized: true, 
+  resave: true 
+}));
+router.use(passport.initialize());
+router.use(passport.session());
+
 const {
   getAllRooms,
   filterRooms
 } = require("../utils/filters/loadRooms");
 
-const bodyParser = require("body-parser");
-router.use(bodyParser.urlencoded({ extended: true }));
+const { 
+  isLoggedIn 
+} = require("../utils/register/authentication");
 
 
-
-router.get("/", async (req, res) => {
+router.get("/", isLoggedIn, async (req, res) => {
   const allRooms = await getAllRooms();
 
   // ophalen rooms database
