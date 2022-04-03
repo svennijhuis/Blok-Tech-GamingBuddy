f4c2499c68dfa30dda55b81c5cd7c39876657e77
svennijhuis <sanijhuis@live.nl>
2022-04-03 20:50:17 +0200

    [added] information

---
+++ utils/information/personInformation.js
@@ -0,0 +1,22 @@
+const mongoConnect = require("../../controller/mongoConnect");
+
+// verander room info (moet nog aan gewerkt worden)
+const changeUser = async (newUserData, name) => {
+    const client = await mongoConnect.getDB();
+
+    // naam van room (moet nog dynamisch worden meegegeven)
+    const userData = { name: name };
+
+    // object keys die moeten worden aangepast + nieuwe waardes
+    const update = {
+        $set: {
+          name:newUserData.name,
+        }
+      };
+
+    await client.db("users")
+    .collection("user")
+    .updateOne(userData, update);
+};
+
+module.exports = { changeUser };
