6ebb0b69e66d86dd5a43bae5d1bfecb3f03cb5a4
Nina Vens <nina.vens@hva.nl>
2022-04-03 13:50:46 +0200

    [added] styling

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+
+// helper functions for Passport and database
+const { dbReg, dbAuth } = require("./utils/register/authentication");
+
+
+
+//===============DATABASE===============
+const { loadChat, saveChat, deleteChat } = require("./utils/io/chatActions.js");
+const mongoConnect = require("./controller/mongoConnect");
+let client;
+
+
+
+//===============SOCKET.IO==============
 const server = require("http").createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -12,11 +33,7 @@ const cors = require("cors");
 app.use(cors({ origin: "*" }));
 instrument(io, { auth: false });
 
-// database functions
-const { loadChat, saveChat, deleteChat } = require("./utils/io/chatActions.js");
-const mongoConnect = require("./controller/mongoConnect");
-let client;
-
+// helper function for formatting messages
 const { formatMessage } = require("./utils/io/messages.js");
 
 // houdt users in room bij
@@ -35,102 +52,9 @@ const { sendWelcomeEmail } = require("./utils/email/email.js");
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
-const exphbs = require("express-handlebars");
-app.engine(
-  "hbs",
-  exphbs.engine({
-    defaultLayout: "index",
-    extname: ".hbs",
-  })
-);
-
-app.set("view engine", "hbs");
-
-const flash = require("express-flash");
-app.use(flash());
-
-//===============ROUTES===============
-app.use("/", require("./routes/roomSelect"));
-app.use("/messages", require("./routes/chat"));
-app.use("/register", require("./routes/test"));
-app.use("/login", require("./routes/login"));
-app.use("/logout", require("./routes/logout"));
-
-io.on("connect", (socket) => {
-  socket.on("joinRoom", async ({ username, room }) => {
-    // wordt uitgevoerd wanneer gebruiker room joined, user object wordt in users array gezet voor sidebar info (utils/users.js)
-    const user = userJoin(socket.id, username, room);
-
-    socket.join(user.room);
-
-    // haal chat op uit database
-    loadChat(client, user.room, socket);
-
-    // kijk of kamer metadata bestaat, anders wordt die aangemaakt
-    await checkRoomData(client, room);
-
-    const roomData = await loadRoomData(client, room);
-
-    // toont room metadata in sidebar popup
-    io.to(user.room).emit("loadedRoomData", roomData);
-
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        "systemMessage",
-        formatMessage("Server", `${user.username} has joined the chat`)
-      );
-
-    // update users in sidebar
-    io.to(user.room).emit("updateusers", getRoomUsers(user.room));
-
-    socket.on("message", (msg) => {
-      // chat message van user
-      const user = getCurrentUser(socket.id);
-
-      // voeg metadata aan message object toe (tijd, id etc.)
-      const fullMsg = formatMessage(user, msg);
-
-      io.to(user.room).emit("message", fullMsg);
-
-      // sla message object op in database
-      saveChat(client, fullMsg);
-    });
-
-    socket.on("deleteMsg", (room, messageId) => {
-      // verwijder message globaal, zowel in room als database
-      deleteChat(client, room, messageId);
-      io.to(user.room).emit("deleteMsgGlobal", messageId);
-    });
-
-    socket.on("disconnect", () => {
-      // update sidebar en emit "has left the chat" msg
-      const user = userLeave(socket.id);
-
-      // emit message als er nog users in room zijn
-      if (user) {
-        io.to(user.room).emit(
-          "systemMessage",
-          formatMessage("Server", `${user.username} has left the chat`)
-        );
-
-        io.to(user.room).emit("updateusers", getRoomUsers(user.room));
-      }
-    });
-  });
-});
-//////////////////////////////
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const methodOverride = require("method-override");
 
 
-const { dbReg, dbAuth } = require("./utils/register/authentication");
-
-// Passport.js
+//===============PASSPORT===============
 passport.serializeUser((user, done) => {
   console.log("serializing " + user.username);
   done(null, user);
@@ -196,7 +120,10 @@ passport.use(
   )
 );
 
-// express
+
+
+//===============EXPRESS================
+// Configure Express
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 
@@ -204,6 +131,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
+app.use(flash());
 app.use(
   session({
     secret: "process.env.SESSION_SECRET",
@@ -232,10 +160,100 @@ app.use((req, res, next) => {
   next();
 });
 
+// configure express to use handlebars templates
+const exphbs = require("express-handlebars");
+app.engine(
+  "hbs",
+  exphbs.engine({
+    defaultLayout: "index",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+
+
+
+//=========SOCKET.IO CONNECTION=========
+io.on("connect", (socket) => {
+  socket.on("joinRoom", async ({ username, room }) => {
+    // wordt uitgevoerd wanneer gebruiker room joined, user object wordt in users array gezet voor sidebar info (utils/users.js)
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // haal chat op uit database
+    loadChat(client, user.room, socket);
+
+    // kijk of kamer metadata bestaat, anders wordt die aangemaakt
+    await checkRoomData(client, room);
+
+    const roomData = await loadRoomData(client, room);
+
+    // toont room metadata in sidebar popup
+    io.to(user.room).emit("loadedRoomData", roomData);
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "systemMessage",
+        formatMessage("Server", `${user.username} has joined the chat`)
+      );
+
+    // update users in sidebar
+    io.to(user.room).emit("updateusers", getRoomUsers(user.room));
+
+    socket.on("message", (msg) => {
+      // chat message van user
+      const user = getCurrentUser(socket.id);
+
+      // voeg metadata aan message object toe (tijd, id etc.)
+      const fullMsg = formatMessage(user, msg);
+
+      io.to(user.room).emit("message", fullMsg);
+
+      // sla message object op in database
+      saveChat(client, fullMsg);
+    });
+
+    socket.on("deleteMsg", (room, messageId) => {
+      // verwijder message globaal, zowel in room als database
+      deleteChat(client, room, messageId);
+      io.to(user.room).emit("deleteMsgGlobal", messageId);
+    });
+
+    socket.on("disconnect", () => {
+      // update sidebar en emit "has left the chat" msg
+      const user = userLeave(socket.id);
+
+      // emit message als er nog users in room zijn
+      if (user) {
+        io.to(user.room).emit(
+          "systemMessage",
+          formatMessage("Server", `${user.username} has left the chat`)
+        );
+
+        io.to(user.room).emit("updateusers", getRoomUsers(user.room));
+      }
+    });
+  });
+});
+
+
+
 app.use((req, res, next) => {
   app.locals.success = req.flash("success");
   next();
 });
+
+//===============ROUTES===============
+app.use("/", require("./routes/roomSelect"));
+app.use("/messages", require("./routes/chat"));
+app.use("/register", require("./routes/test"));
+app.use("/login", require("./routes/login"));
+app.use("/logout", require("./routes/logout"));
+
+
 
 //========DATABASE CONNECTION=========
 const startServer = async () => {
@@ -246,6 +264,8 @@ const startServer = async () => {
 };
 
 startServer();
+
+
 
 //===============ERROR================
 app.use((req, res, next) => {
