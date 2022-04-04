6ae452fce8e9ec472fec7f82678bf081ed546a27
Nina Vens <nina.vens@hva.nl>
2022-04-04 23:16:51 +0200

    [added] password hashing

--- routes/account.js
+++ routes/account.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const mongoConnect = require("../controller/mongoConnect");
 
+const bcrypt = require("bcryptjs");
+
 const express = require("express");
 const router = express.Router();
 
@@ -15,6 +17,15 @@ router.use(session({
 router.use(passport.initialize());
 router.use(passport.session());
 
+router.use((req, res, next) => {
+    const error = req.session.error;
+  
+    delete req.session.error;
+  
+    if (error) res.locals.error = error;
+  
+    next();
+});
 
 // render account
 router.get("/", (req, res) => {
@@ -28,25 +39,38 @@ router.get("/", (req, res) => {
 router.post("/", async (req,res) => {
     const client = await mongoConnect.getDB();
     
-    client
-    .db("users")
-    .collection("user")
-    .updateOne(
-      { name: req.user.name,
-        email: req.user.email,
-        age: req.user.age,
-        residence: req.user.residence, 
-        username: req.user.username
-      }, 
-      {$set: {
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age,
-        residence: req.body.residence, 
-        username: req.body.username
-      }}
-    ) 
-    res.redirect("/");
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    const verification = bcrypt.compareSync(req.body.confirm_password, hash);
+    
+    if (verification) {
+        client
+        .db("users")
+        .collection("user")
+        .updateOne(
+        { name: req.user.name,
+            email: req.user.email,
+            age: req.user.age,
+            residence: req.user.residence, 
+            username: req.user.username,
+            password: req.user.password,
+            confirm_password: req.user.confirm_password
+        }, 
+        {$set: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+            residence: req.body.residence, 
+            username: req.body.username,
+            password: hash,
+            confirm_password: verification
+        }}
+        ) 
+        res.redirect("/");
+    } else {
+        console.log("fail");
+    }
+    
+    
 });
 
 module.exports = router;
