7e1867b89782b2c571d86dbfa63d3b8856636a6c
Laurens256 <duinlaurens2@gmail.com>
2022-04-04 10:56:17 +0200

    [bugfix] fixed merge conflicts

--- routes/chat.js
+++ routes/chat.js
@@ -5,6 +5,10 @@ const uniqid = require("uniqid");
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
+const {
+  isLoggedIn
+} = require("../utils/register/authentication");
+
 const multer = require("multer");
 
 const storage = multer.diskStorage({
@@ -12,7 +16,7 @@ const storage = multer.diskStorage({
         callback(null, "public/uploads/");
     },
     filename: function (request, file, callback) {
-        callback(null, `${uniqid()}-${file.originalname}`);
+        callback(null, `${file.originalname}-${uniqid()}`);
     }
 });
 
@@ -23,7 +27,7 @@ const { changeRoomInfo } = require("../utils/filters/changeRoomInfo");
 let roomNaam;
 
 // render chat
-router.get("/", (req, res) => {
+router.get("/", isLoggedIn, (req, res) => {
   res.render("chat", {
     groepsnaam: req.query.room
     // groepsnaam: req.query.room.charAt(0).toUpperCase() + req.query.room.slice(1)
@@ -32,22 +36,18 @@ router.get("/", (req, res) => {
 });
 
 // stuur user naar chat met username en room als query parameters
-router.post("/", (req, res) => {
+router.post("/", isLoggedIn, (req, res) => {
   res.redirect(`/messages?username=${req.body.username}&room=${req.body.room}`);
 });
 
-// Hiermee kan je de huidige room info als gebruiker veranderen
-router.post("/roomimg", upload.single("groepimg"), function (req, res) {
-  // huidige data vervangen met newRoomData
+// upload custom groep afbeelding
+// moet nog error handling hebben, bijvoorbeeld als bepaalde velden niet worden ingevuld. Dit is de basis - Laurens
+router.post("/roomimg", isLoggedIn, upload.single("groepimg"), function (req, res) {
   const newRoomData = {
-    beschrijving: req.body.beschrijving,
-    taal: [req.body.taal1, req.body.taal2, req.body.taal3]
+    omschrijving: req.body.omschrijving,
+    taal: [req.body.taal1, req.body.taal2, req.body.taal3],
+    img: `uploads/${req.file.filename}`
   };
-
-  // Als geen img is toegevoegd dan de huidige img behouden.
-  if (req.file !== undefined) {
-    newRoomData.img = `uploads/${req.file.filename}`;
-  }
 
   changeRoomInfo(newRoomData, roomNaam);
   res.status(204).end();
