10e031d5a3f41002a3d0a9653c06a31b3e369c13
Nina Vens <nina.vens@hva.nl>
2022-04-05 22:18:37 +0200

    [removed] unused code

--- public/js/scriptFrontend.js
+++ public/js/scriptFrontend.js
@@ -1,38 +1,3 @@
-// ========= modal box - personal information =========
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelector(".show-modal");
-
-const hiddenModal = () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-const openModal = () => {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-try {
-  btnsOpenModal.addEventListener("click", () => {
-    openModal();
-  });
-
-  btnCloseModal.addEventListener("click", () => {
-    hiddenModal();
-  });
-
-  overlay.addEventListener("click", () => {
-    hiddenModal();
-  });
-
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-      openModal();
-    }
-  });
-} catch (err) {}
 
 // ========= API - IntersectionObserver  =========
 
