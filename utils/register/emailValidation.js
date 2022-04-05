5c446f6c17f6ff0c32f92c9c13dc8f10699c9e08
Nina Vens <nina.vens@hva.nl>
2022-04-05 12:55:00 +0200

    [added] email validation

--- utils/register/emailValidation.js
+++ utils/register/emailValidation.js
@@ -1,8 +1,6 @@
-
-
 const emailVal = (email) => {
 
-    var valid = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const valid = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
     if (!valid.test(email.value)) {
         console.log("false email");
